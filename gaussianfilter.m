5835fd891d778f0ca03bf5a982e2d075727f0dba
Alex Rothberg <agrothberg@gmail.com>
2017-11-12 23:16:38 -0500

    Initial import of paper code

---
+++ gaussianfilter.m
@@ -0,0 +1,26 @@
+%%% Input:  The variance of the noise
+%%% Output: The smoothing 2D gaussian filter 
+
+% The steps for the construction of the 2D gaussian filter are described in 
+% "M. Petrou and C. Petrou, Image Processing: The Fundamentals, John Wiley 2010".
+
+
+function gfilter=gaussianfilter(snoise)
+
+ey=3*snoise;
+M=snoise*sqrt(-2*log(ey));
+
+
+
+i=0;
+for x=-2*M:M:2*M
+    j=0;
+    i=i+1;
+    for y=-2*M:M:2*M
+        j=j+1;
+gfilter(i,j)=exp(-(x^2+y^2)/(2*(snoise^2)));
+    end
+end
+gfilter=gfilter/(sum(sum(gfilter)));
+
+end
---
+++ noise_estimate.m
@@ -0,0 +1,25 @@
+%%% Input:  Noisy image A
+%%% Output: The estimate of the standard deviation of the noise
+
+
+function snoise=noise_estimate(A)
+
+
+
+m=size(A,1);
+
+% A sliding window of size relative to the width of the image is used 
+if mod(round(m/5),2) == 1
+    pixNHOOD=round(m/5);
+else
+    pixNHOOD=round(m/5)-1;
+end
+
+NHOOD=ones(pixNHOOD);
+
+% The intensity standard deviation of the most uniform window is used as the 
+% estimate of the standard deviation of the noise
+J = stdfilt(A, NHOOD);
+snoise=min(min(J));
+
+end
